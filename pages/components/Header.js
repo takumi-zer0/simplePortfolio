76abf996cdbe9ad6718381519d5ca90debbc9a7b
Takumi Ueno <takumiueno@Takumis-MacBook-Air.local>
2022-03-04 09:12:07 +0900

    tests

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
       {/* Code block starts */}
       <div className="my-6 lg:my-12 container px-6 mx-auto flex flex-col md:flex-row items-center md:items-center justify-center pb-4 border-b border-gray-300">
         <div>
-          <h4 className="text-2xl font-bold leading-tight text-black text-center">Takumi Ueno's Portfolio</h4>
+          <h4 className="text-2xl font-bold leading-tight text-black text-center">Takumi Ueno</h4>
         </div>
         {/* <div className="mt-6 md:mt-0">
           <button className="mr-3 bg-gray-200 focus:outline-none transition duration-150 ease-in-out rounded hover:bg-gray-300 text-indigo-700  px-5 py-2 text-sm">Back</button>
